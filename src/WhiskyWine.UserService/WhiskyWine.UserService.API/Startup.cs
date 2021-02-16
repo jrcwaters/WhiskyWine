88dd243d31cda501ccd449c2baf96719f4d65b09
Jack <jackwaters2@icloud.com>
2021-02-16 16:54:58 +0000

    Added class libraries for Data and domain

--- src/WhiskyWine.UserService/WhiskyWine.UserService.API/Startup.cs
+++ src/WhiskyWine.UserService/WhiskyWine.UserService.API/Startup.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.DataProtection.Repositories;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace WhiskyWine.UserService.API
 {
@@ -16,6 +18,21 @@ namespace WhiskyWine.UserService.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+            services.AddMemoryCache();
+            services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Version = "v1",
+                Title = "WhiskyWine User Service API",
+                Description = "API for user service in WhiskyWine App",
+                Contact = new OpenApiContact
+                {
+                    Name = "Jack Waters", Email = "jackwaters2@icloud.com"
+                }
+            }));
+
+            services.AddTransient<IUserService, UserService>();
+            services.AddTransient<IRepository<User>, UserInMemoryRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
